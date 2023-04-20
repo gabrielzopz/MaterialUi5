294beef80310b7171acd614543df323e42b81526
gabrielzopz <gabrielz.opz@gmail.com>
2023-04-20 09:26:27 -0300

    feat: Adicionado tema escuro e troca de tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
+import { AppThemeProvider } from "./shared/contexts";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from './routes';
-import { ThemeProvider } from "@emotion/react";
-import { LightTheme } from "./shared/themes";
+
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
+    <AppThemeProvider>
       <BrowserRouter>
-          <AppRoutes />
+        <AppRoutes />
       </BrowserRouter>
-    </ThemeProvider>
+    </AppThemeProvider>
   );
 }
 
